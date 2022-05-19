e47654fa4eafa42db651fd89c885037af42a49b1
kaacpereek <kacperkukula97@gmail.com>
2022-05-19 15:07:55 +0200

    create using spring

---
+++ maven.java.conference/src/test/java/com/project/maven/java/conference/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.maven.java.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
